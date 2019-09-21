011ab263e0acd5fbd76eaa9f2a510159bc0b9d28
Stefan Alfons <stefan.alfons@icloud.com>
2019-09-21 15:46:00 +0200

    Created Folder structure.
    Signed-off-by: Stefan Alfons <stefan.alfons@icloud.com>

---
+++ src/BIF/SWE1/UEB1.java
@@ -0,0 +1,16 @@
+package BIF.SWE1;
+
+import BIF.SWE1.interfaces.Url;
+
+import java.util.Map;
+
+public class UEB1 {
+
+	public Url getUrl(String path) {
+
+		return null; //TODO: memes
+	}
+
+	public void helloWorld() {
+	}
+}
